546d1594b8a81c5165d5f500350f0e06e7cfdb2c
LaviniaGalan <galan.lavinia14@gmail.com>
2021-03-30 15:22:52 +0300

    Separating Login functionality on layers.

--- WPF Client/LoginWindow.xaml.cs
+++ WPF Client/LoginWindow.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Microsoft.Data.SqlClient;
+using Elearning.Business;
+using System.Data.SqlClient;
 
 namespace ElearningClient
 {
@@ -32,13 +33,19 @@ namespace ElearningClient
             {
                 if (conn.State == ConnectionState.Closed)
                 { 
-                    conn.Open(); 
+                    conn.Open();
+                    Login login = new Login();
+                    String username = UsernameTxt.Text;
+                    String password = PasswordTxt.Text;
+                    login.ValidateLogin(conn, username, password);
+                    this.ShowMainWindow();
+                    
                 }
-
-                CheckLogin(conn);
             }
             catch(Exception ex)
             {
+                UsernameTxt.Clear();
+                PasswordTxt.Clear();
                 MessageBox.Show(ex.Message);
             }
             finally
@@ -47,31 +54,11 @@ namespace ElearningClient
             }
         }
 
-        private void CheckLogin(SqlConnection conn)
+        public void ShowMainWindow()
         {
-            String username = UsernameTxt.Text;
-            String password = PasswordTxt.Text;
-
-            String query = "SELECT COUNT(*) FROM Users " +
-                "WHERE (Username = @Username AND Password = @Password) OR " +
-                "(Email = @Username AND Password = @Password)";
-
-            SqlCommand command = new SqlCommand(query, conn);
-            command.CommandType = CommandType.Text;
-            command.Parameters.AddWithValue("@Username", username);
-            command.Parameters.AddWithValue("@Password", password);
-            int count = Convert.ToInt32(command.ExecuteScalar());
-
-            if (count == 1)
-            {
-                MainWindow mainWindow = new MainWindow();
-                mainWindow.Show();
-                this.Close();
-            }
-            else
-            {
-                MessageBox.Show("Invalid credentials!");
-            }
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
 
         private void RegisterBtn_Click(object sender, RoutedEventArgs e)
