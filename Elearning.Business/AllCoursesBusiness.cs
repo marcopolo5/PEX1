0406a8f339ed93a35db3bb2e7587aaa611c24b56
LaviniaGalan <galan.lavinia14@gmail.com>
2021-04-08 10:34:08 +0300

    Added course DTO

--- Elearning.Business/AllCoursesBusiness.cs
+++ Elearning.Business/AllCoursesBusiness.cs
@@ -8,47 +8,61 @@ using System;
 
 namespace Elearning.Business
 {
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DifficultyEnum Difficulty { get; set; }
+
+    }
     public class AllCoursesBusiness
     {
-        public List<Course> GetAllCoursesOfAnUser(User user)
+        public List<CourseDTO> GetAllCoursesOfAnUser(User user)
         {
             using (ElearningContext context = new ElearningContext())
             {
-                //try
-                //{
-                    var usersCourses = context.UserProgresses
-                                      .Where(enrollment => enrollment.UserId == user.Id)
-                                      .Select(enrollment => enrollment.Course)
-                    .ToList();
+                var usersCourses = (from userProgress in context.UserProgresses
+                                    where userProgress.UserId == user.Id
+                                    select new CourseDTO()
+                                    {
+                                        Id = userProgress.Course.Id,
+                                        Name = userProgress.Course.Name,
+                                        Description = userProgress.Course.Description,
+                                        Category = userProgress.Course.Category,
+                                        Difficulty = userProgress.Course.Difficulty
+                                    }).ToList();
                     return usersCourses;
-                //}
-                //catch (Exception exception)
-                //{
-                //    return new List<Course>();
-                //}
-                //TODO: handling exceptions?
             }
         }
 
-        public List<Course> GetSuggestedCoursesForAnUser(User user)
+        public List<CourseDTO> GetSuggestedCoursesForAnUser(User user)
         {
             using (ElearningContext context = new ElearningContext())
             {
-                //try
-                //{
-                    var usersCourses = context.UserProgresses
-                                      .Where(enrollment => enrollment.UserId == user.Id)
-                                      .Select(enrollment => enrollment.Course);
-                    var allCourses = context.Courses
-                                        .Select(course => course);
+                var usersCourses = from userProgress in context.UserProgresses
+                                   where userProgress.UserId == user.Id
+                                   select new CourseDTO()
+                                   {
+                                       Id = userProgress.Course.Id,
+                                       Name = userProgress.Course.Name,
+                                       Description = userProgress.Course.Description,
+                                       Category = userProgress.Course.Category,
+                                       Difficulty = userProgress.Course.Difficulty
+                                   };
+                var allCourses = from course in context.Courses
+                                 select new CourseDTO()
+                                 {
+                                     Id = course.Id,
+                                     Name = course.Name,
+                                     Description = course.Description,
+                                     Category = course.Category,
+                                     Difficulty = course.Difficulty
+                                 };
 
-                    var suggestedCourses = allCourses.Except(usersCourses).ToList();
-                    return suggestedCourses;
-                //}
-                //catch (Exception exception)
-                //{
-                //    return new List<Course>();
-                //}
+                var suggestedCourses = allCourses.Except(usersCourses).ToList();
+                return suggestedCourses;
 
             }
         }
