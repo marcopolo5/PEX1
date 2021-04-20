b7f96f59eae362c20a148e2f22d95b4ef8d5cd7f
LaviniaGalan <72808972+LaviniaGalan@users.noreply.github.com>
2021-04-20 17:04:23 +0300

    Added enrollment service.

---
+++ Elearning.Business/Services/EnrollmentService.cs
@@ -0,0 +1,32 @@
+﻿using ElearningDatabase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elearning.Business.Services
+{
+    class EnrollmentService
+    {
+        public void AddEnrollment(int userId, int courseId)
+        {
+            try
+            {
+                using (ElearningContext elearningContext = new ElearningContext())
+                {
+                    elearningContext.UserProgresses.Add(new ElearningDatabase.Models.UserProgress()
+                    {
+                        UserId = userId,
+                        CourseId = courseId,
+                        Progress = 0
+                    });
+                    elearningContext.SaveChanges();
+                }
+
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
