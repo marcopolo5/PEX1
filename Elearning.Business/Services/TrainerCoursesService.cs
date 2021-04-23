5be2359a743c770f77c242d3eabb7091ede84247
LaviniaGalan <72808972+LaviniaGalan@users.noreply.github.com>
2021-04-23 12:00:31 +0300

    Added service for trainer's courses.

---
+++ Elearning.Business/Services/TrainerCoursesService.cs
@@ -0,0 +1,60 @@
+﻿using ElearningDatabase;
+using ElearningDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Elearning.Business.Services
+{
+    public class TrainerCoursesService
+    {
+        public List<Course> GetAllCoursesOfATrainer(User trainer)
+        {
+            using (ElearningContext context = new ElearningContext())
+            {
+                var usersCourses = (from course in context.Courses
+                                    where course.AuthorId == trainer.Id
+                                    select new Course()
+                                    {
+                                        Id = course.Id,
+                                        Name = course.Name,
+                                        Description = course.Description,
+                                        Category = course.Category,
+                                        Difficulty = course.Difficulty
+                                    }).ToList();
+                return usersCourses;
+            }
+        }
+
+        public List<Course> GetSuggestedCoursesForAnUser(User author)
+        {
+            using (ElearningContext context = new ElearningContext())
+            {
+                var usersCourses = from course in context.Courses
+                                   where course.AuthorId == author.Id
+                                   select new Course()
+                                   {
+                                       Id = course.Id,
+                                       Name = course.Name,
+                                       Description = course.Description,
+                                       Category = course.Category,
+                                       Difficulty = course.Difficulty
+                                   };
+                var allCourses = from course in context.Courses
+                                 select new Course()
+                                 {
+                                     Id = course.Id,
+                                     Name = course.Name,
+                                     Description = course.Description,
+                                     Category = course.Category,
+                                     Difficulty = course.Difficulty
+                                 };
+
+                var suggestedCourses = allCourses.Except(usersCourses).ToList();
+                return suggestedCourses;
+
+            }
+        }
+    }
+}
