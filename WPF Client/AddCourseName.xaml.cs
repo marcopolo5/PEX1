497315baa525627075901ee9cce43b1d7d436654
gosicker <53013900+gosicker@users.noreply.github.com>
2021-04-27 13:00:50 +0300

    Combo boxurile sunt gata

---
+++ WPF Client/AddCourseName.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Client
+{
+    /// <summary>
+    /// Interaction logic for AddCourseName.xaml
+    /// </summary>
+    public partial class AddCourseName : Window
+    {
+        public AddCourseName()
+        {
+            InitializeComponent();
+        }
+    }
+}
