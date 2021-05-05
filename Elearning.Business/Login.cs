504ea2364745d430163a8902d7adee0bbdd0f5c6
LaviniaGalan <72808972+LaviniaGalan@users.noreply.github.com>
2021-05-05 17:37:52 +0300

    Fixes on register.

--- Elearning.Business/Login.cs
+++ Elearning.Business/Login.cs
@@ -10,23 +10,7 @@ namespace Elearning.Business
 {
     public class Login
     {
-        public void ValidateLogin(SqlConnection conn, string username, string password)
-        {
-            string query = "SELECT COUNT(*) FROM Users " +
-                "WHERE (Username = @Username AND Password = @Password) OR " +
-                "(Email = @Username AND Password = @Password)";
-
-            SqlCommand command = new SqlCommand(query, conn);
-            command.CommandType = CommandType.Text;
-            command.Parameters.AddWithValue("@Username", username);
-            command.Parameters.AddWithValue("@Password", password);
-            int count = Convert.ToInt32(command.ExecuteScalar());
-
-            if (count != 1)
-            {
-                throw new Exception("Invalid credentials!");
-            }
-        }
+       
 
         public User GetLoggedUser(string username, string password)
         {
